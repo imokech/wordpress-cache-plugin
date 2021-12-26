3aa519547bb7473ffdef209197e0b967d8473279
imokech <choobeh77@gmail.com>
2021-12-26 11:27:13 +0330

    add - page abstract and management

--- panel/Management.php
+++ panel/Management.php
@@ -1,15 +1,16 @@
 <?php
 
-
 include_once CI_CACHE . '/panel/Page.php';
 include_once CI_CACHE . '/includes/Enum.php';
+
+if (!defined('ABSPATH')) exit();
 
 class Management extends Page
 {
     public function __construct()
     {
-        $this->pageTitle    = __('Cache Management', Text_Domain);
-        $this->menuTitle    = __('Cache Management', Text_Domain);
+        $this->pageTitle    = __('Cinnamon', Text_Domain);
+        $this->menuTitle    = __('Cinnamon', Text_Domain);
         $this->capability   = 'manage_options';
         $this->menuSlug     = 'cinnamon_cache_management';
 
