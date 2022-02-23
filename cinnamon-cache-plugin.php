9edb93bfc5aa27e5094aa890d207b9c4d7ee6298
imokech <choobeh77@gmail.com>
2022-02-23 23:51:42 +0330

    change - start plugin file name

--- cinnamon-cache-plugin.php
+++ cinnamon-cache-plugin.php
@@ -7,19 +7,19 @@
  * @copyright Copyright © 2021 Mokech
  *
  * Plugin Name:       Cinnamon Cache
- * Plugin URI:        https://mokech.ir
+ * Plugin URI:        https://mokech.ir/plugins/cinnamon-cache-plugin
  * Description:       Cinnamon Cache 
  * Version:           0.1.0
  * Author:            Mokech
  * Author URI:        https://mokech.ir
- * Text Domain:       Ci_Cache
+ * Text Domain:       cinnamon-cache
  * License URI:       LICENSE.txt
  * Domain Path:       /languages
  * Tested up to:      5.9
  */
 
 use Cinnamon\Handlers\CacheHandler;
-use Cinnamon\Includes\Enum;
+use Cinnamon\Includes\CinnamonCachePluginEnum;
 use Cinnamon\Panel\Management;
 
 if (!defined('ABSPATH')) exit();
@@ -27,7 +27,7 @@ if (!defined('ABSPATH')) exit();
 
 function CinnamonCachePlugin ()
 {
-    class Cache
+    class CinnamonCachePlugin
     {
         public function __construct()
         {
@@ -41,7 +41,6 @@ function CinnamonCachePlugin ()
         {
             define('CI_CACHE', plugin_dir_path(__FILE__));
             define('CI_CACHE_URI', plugin_dir_url(__FILE__));
-            define('Text_Domain', 'Ci_Cache');
         }
         
         public function init()
@@ -65,8 +64,8 @@ function CinnamonCachePlugin ()
         {
             $args = array(
                 'id'    => 'ci_cache_item',
-                'title' => __('Flush All Cache', Text_Domain),
-                'href'  => wp_nonce_url(admin_url() . 'admin.php?page=' . Enum::PAGE_SLUG, -1, Enum::NONCE_NAME_PURGE),
+                'title' => __('Flush All Cache', 'cinnamon-cache'),
+                'href'  => wp_nonce_url(admin_url() . 'admin.php?page=' . CinnamonCachePluginEnum::PAGE_SLUG, -1, CinnamonCachePluginEnum::NONCE_NAME_PURGE),
             );
             $wpAdminBar->add_node( $args );
         }   
@@ -78,13 +77,13 @@ function CinnamonCachePlugin ()
         
         private function loader()
         {
-            include_once CI_CACHE . 'includes/Enum.php';
+            include_once CI_CACHE . 'includes/CinnamonCachePluginEnum.php';
             include_once CI_CACHE . 'panel/Management.php';
             include_once CI_CACHE . 'handlers/CacheHandler.php';
         }
     }
 
-    $cacheInstance = new Cache;
+    $cacheInstance = new CinnamonCachePlugin;
 }
 
 add_action('init', 'CinnamonCachePlugin');
