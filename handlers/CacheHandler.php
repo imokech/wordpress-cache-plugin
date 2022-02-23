ba72db8d01bb1b36e795228d144bbacde8edfa3d
imokech <choobeh77@gmail.com>
2022-02-23 23:53:10 +0330

    fix - find current id

--- handlers/CacheHandler.php
+++ handlers/CacheHandler.php
@@ -3,11 +3,11 @@
 namespace Cinnamon\Handlers;
 
 use Cinnamon\Includes\CinnamonCache;
-use Cinnamon\Includes\Enum;
+use Cinnamon\Includes\CinnamonCachePluginEnum;
 use Exception;
 
 include_once CI_CACHE . '/includes/CinnamonCache.php';
-include_once CI_CACHE . '/includes/Enum.php';
+include_once CI_CACHE . '/includes/CinnamonCachePluginEnum.php';
 
 if (!defined('ABSPATH')) exit();
 
@@ -31,25 +31,25 @@ class CacheHandler
             isset($_POST['cache_time']) 
             && !empty($_POST['cache_time']) 
             && isset($_POST['set_cache_time']) 
-            && wp_verify_nonce($_REQUEST[Enum::NONCE_NAME_SETTING], Enum::NONCE_NAME_SETTING)
+            && wp_verify_nonce($_REQUEST[CinnamonCachePluginEnum::NONCE_NAME_SETTING], CinnamonCachePluginEnum::NONCE_NAME_SETTING)
         ) {
             $cacheTime = (preg_match("/^[0-9]{1,8}$/", $_POST['cache_time'])) ? 
-                         $_POST['cache_time'] : Enum::CACHE_MAX_TIME;
+                         $_POST['cache_time'] : CinnamonCachePluginEnum::CACHE_MAX_TIME;
                          
-            update_option(Enum::CACHE_TIME_META, $cacheTime);
+            update_option(CinnamonCachePluginEnum::CACHE_TIME_META, $cacheTime);
         }
 
         if (
             (
                 isset($_POST['flush_all_cache']) 
                 && 
-                wp_verify_nonce($_REQUEST[Enum::NONCE_NAME_PURGE], Enum::NONCE_NAME_PURGE)
+                wp_verify_nonce($_REQUEST[CinnamonCachePluginEnum::NONCE_NAME_PURGE], CinnamonCachePluginEnum::NONCE_NAME_PURGE)
             )
             ||
             (
-                $_GET['page'] == Enum::PAGE_SLUG
+                $_GET['page'] == CinnamonCachePluginEnum::PAGE_SLUG
                 &&
-                check_admin_referer(-1, Enum::NONCE_NAME_PURGE)
+                check_admin_referer(-1, CinnamonCachePluginEnum::NONCE_NAME_PURGE)
             )
         ) {
             $this->cacheObj->flush();
@@ -58,12 +58,13 @@ class CacheHandler
 
     public function callback($buffer) 
     {
-        global $post;
-        $key = $post->ID ?? get_queried_object_id();
+        $scheme     = (!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] === "off") ? "http" : "https";
+        $url        = "$scheme://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $key        = url_to_postid( $url );
 
-        $cache = $this->cacheObj->get(md5($key));
-        $cacheTime = get_option(Enum::CACHE_TIME_META) ?? (24 * 60 * 60);
-
+        $cache = $this->cacheObj->get($key);
+        $cacheTime = get_option(CinnamonCachePluginEnum::CACHE_TIME_META) ?? (24 * 60 * 60);
+        
         try {
             if ($cache){
                 $buffer = $cache;
