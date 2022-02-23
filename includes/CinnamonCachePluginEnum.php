0e3b03b9aa9d43fd6546382443c7b3ecb0e76929
imokech <choobeh77@gmail.com>
2022-02-23 23:52:44 +0330

    add  - Plugin Enum

--- includes/CinnamonCachePluginEnum.php
+++ includes/CinnamonCachePluginEnum.php
@@ -2,12 +2,12 @@
 
 namespace Cinnamon\Includes;
 
-class Enum {
+class CinnamonCachePluginEnum {
     const NONCE_NAME_PURGE = '_wpnonce_purge';
     const NONCE_NAME_SETTING = '_wpnonce_setting';
     const PAGE_SLUG = 'cinnamon_cache_management';
     const CACHE_FOLDER = 'cache';
     const CACHE_TIME_META = '_ci_cache_time';
     const CACHE_MAX_TIME = 60 * 60 * 24 * 30;
-    const CACHE_DIR = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . Enum::CACHE_FOLDER;
+    const CACHE_DIR = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . CinnamonCachePluginEnum::CACHE_FOLDER;
 }
