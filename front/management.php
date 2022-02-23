200e3b14a9b47a6d9cc5c204d9372ce8bec1c729
imokech <choobeh77@gmail.com>
2022-02-23 23:52:09 +0330

    add - text domain

--- front/management.php
+++ front/management.php
@@ -7,12 +7,12 @@
             </ul>
         </div>
         <form method="post">
-            <button name="flush_all_cache" type="submit"><?php echo __('Flush All Cache', Text_Domain); ?></button>
+            <button name="flush_all_cache" type="submit"><?php echo __('Flush All Cache', 'cinnamon-cache'); ?></button>
             <input type="hidden" id="_wpnonce_purge" name="_wpnonce_purge" value="<?php echo wp_create_nonce('_wpnonce_purge');?>" />
         </form>
         <form method="post" action="">
             <input type="text" name="cache_time" value="<?php echo get_option('_ci_cache_time') ? get_option('_ci_cache_time') : ''; ?>">
-            <button name="set_cache_time" type="submit"><?php echo __('Set Time', Text_Domain); ?></button>
+            <button name="set_cache_time" type="submit"><?php echo __('Set Time', 'cinnamon-cache'); ?></button>
             <input type="hidden" id="_wpnonce_setting" name="_wpnonce_setting" value="<?php echo wp_create_nonce('_wpnonce_setting');?>" />
         </form>
     </div>
