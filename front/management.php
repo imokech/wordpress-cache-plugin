e169103bb005e69ea76f08520edf92810dccf7c0
imokech <choobeh77@gmail.com>
2022-03-02 16:05:04 +0330

    fix - escaping

--- front/management.php
+++ front/management.php
@@ -11,8 +11,8 @@
             <input type="hidden" id="_wpnonce_purge" name="_wpnonce_purge" value="<?php echo wp_create_nonce('_wpnonce_purge');?>" />
         </form>
         <form method="post" action="">
-            <input type="text" name="cache_time" value="<?php echo get_option('_ci_cache_time') ? get_option('_ci_cache_time') : ''; ?>">
-            <button name="set_cache_time" type="submit"><?php echo __('Set Time', 'cinnamon-cache'); ?></button>
+            <input type="text" name="cache_time" value="<?php echo esc_html(get_option('_ci_cache_time')) ? esc_html(get_option('_ci_cache_time')) : ''; ?>">
+            <button name="set_cache_time" type="submit"><?php echo esc_html(__('Set Time', 'cinnamon-cache')); ?></button>
             <input type="hidden" id="_wpnonce_setting" name="_wpnonce_setting" value="<?php echo wp_create_nonce('_wpnonce_setting');?>" />
         </form>
     </div>
